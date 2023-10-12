66a785255a8776b79a66f623b3e184da18925664
SiFou01 <seifbaghdad01@gmail.com>
2023-10-12 16:44:06 +0100

    Add function and buttons to change number of boxes

--- script.js
+++ script.js
@@ -2,6 +2,44 @@ let sketchBox = document.getElementById("sketch-box");
 
 for (let i = 0; i < 256; i++) {
     let boxes = document.createElement("div");
+    boxes.classList.add("boxes");
     boxes.classList.add("boxes16");
     sketchBox.appendChild(boxes);
+}
+
+let boxes = document.querySelectorAll("div.boxes");
+
+let boxes8 = document.getElementById("btn8");
+let boxes16 = document.getElementById("btn16");
+let boxes32 = document.getElementById("btn32");
+
+function changeBoxes(num) {
+    while (sketchBox.firstChild) {
+        sketchBox.removeChild(sketchBox.firstChild);
+    }
+    switch (num) {
+        case 8 :
+            for (let i = 0; i < 64; i++) {
+                let boxes = document.createElement("div");
+                boxes.classList.add("boxes");
+                boxes.classList.add("boxes8");
+                sketchBox.appendChild(boxes);
+            }
+            break;
+        case 16 :
+            for (let i = 0; i < 256; i++) {
+                let boxes = document.createElement("div");
+                boxes.classList.add("boxes");
+                boxes.classList.add("boxes16");
+                sketchBox.appendChild(boxes);
+            }
+            break;
+        case 32 :
+            for (let i = 0; i < 1024; i++) {
+                let boxes = document.createElement("div");
+                boxes.classList.add("boxes");
+                boxes.classList.add("boxes32");
+                sketchBox.appendChild(boxes);
+            }
+    }
 }
